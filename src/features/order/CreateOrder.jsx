220fb3f67d657cae884f5a5e7397f674cf5a31a6
Zala <zala.developer@gmail.com>
2023-11-11 09:11:15 +1030

    initial styling using tailwind

---
+++ src/features/order/CreateOrder.jsx
@@ -0,0 +1,122 @@
+import Button from '../../ui/Button';
+import {
+  Form,
+  redirect,
+  useActionData,
+  useNavigate,
+  useNavigation,
+} from 'react-router-dom';
+import { createOrder } from '../../services/apiRestaurant';
+
+// https://uibakery.io/regex-library/phone-number
+const isValidPhone = (str) =>
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+    str,
+  );
+
+const fakeCart = [
+  {
+    pizzaId: 12,
+    name: 'Mediterranean',
+    quantity: 2,
+    unitPrice: 16,
+    totalPrice: 32,
+  },
+  {
+    pizzaId: 6,
+    name: 'Vegetale',
+    quantity: 1,
+    unitPrice: 13,
+    totalPrice: 13,
+  },
+  {
+    pizzaId: 11,
+    name: 'Spinach and Mushroom',
+    quantity: 1,
+    unitPrice: 15,
+    totalPrice: 15,
+  },
+];
+
+function CreateOrder() {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
+  const formErrors = useActionData();
+
+  // const [withPriority, setWithPriority] = useState(false);
+  const cart = fakeCart;
+
+  return (
+    <div>
+      <h2>Ready to order? Lets go!</h2>
+
+      <Form method="POST">
+        <div>
+          <label>First Name</label>
+          <input className="input" type="text" name="customer" required />
+        </div>
+
+        <div>
+          <label>Phone number</label>
+          <div>
+            <input className="input" type="tel" name="phone" required />
+          </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
+        </div>
+
+        <div>
+          <label>Address</label>
+          <div>
+            <input className="input" type="text" name="address" required />
+          </div>
+        </div>
+
+        <div>
+          <input
+            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring-yellow-400 focus:ring-offset-2"
+            type="checkbox"
+            name="priority"
+            id="priority"
+            // value={withPriority}
+            // onChange={(e) => setWithPriority(e.target.checked)}
+          />
+          <label htmlFor="priority">Want to yo give your order priority?</label>
+        </div>
+
+        <div>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <Button disabled={isSubmitting}>
+            {isSubmitting ? 'Placing order...' : 'Order now'}
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+}
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === 'on',
+  };
+
+  const errors = {};
+
+  if (!isValidPhone(order.phone)) {
+    errors.phone =
+      'Please give us your correct phone number. We might need it to contact you.';
+  }
+
+  if (Object.keys(errors).length > 0) return errors;
+
+  // const newOrder = await createOrder(order);
+
+  // return redirect(`/order/${newOrder.id}`);
+  return null;
+}
+
+export default CreateOrder;
